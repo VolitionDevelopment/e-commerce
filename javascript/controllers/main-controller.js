7ffecb4b7f0198af24b93d3c7e3def128dc14d60
Jackson Yeager <jacksonyeager@gmail.com>
2016-09-12 13:53:52 -0400

    Initial commit

---
+++ javascript/controllers/main-controller.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Jackson on 9/12/16.
+ */
+app.controller('mainCtrl', $scope => {
+
+});
---
+++ javascript/ecommerce.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Jackson on 9/12/16.
+ */
+let app = angular.module('ecomerce', ['ngMaterial', 'ngRoutes']);
